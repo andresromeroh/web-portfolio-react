a0dc7faa7878a3d57445b8d5599347ca73c2e2b4
Andresromeroh <andres.romero@firstfactory.com>
2020-12-12 17:03:10 -0600

    Mapped contact me form fields to component state

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,13 +8,18 @@ export default class Contact extends Component {
         super(props);
         this.state = {
             isSending: false,
-            subject: 'Choose Subject',
+            subject: '',
             email: '',
             message: ''
         };
     }
 
+    onInputChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
     render() {
+        const { subject, email, message } = this.state;
         return (
             <Container className='portfolio-block'>
                 <Row>
@@ -26,24 +31,43 @@ export default class Contact extends Component {
                             <Form>
                                 <Form.Group controlId='formBasicEmail'>
                                     <Form.Label>Subject</Form.Label>
-                                    <Form.Control as='select' className='browser-default custom-select'>
-                                        <option>Business Inquiry</option>
-                                        <option>Freelance Project</option>
-                                        <option>Just to say hello!</option>
-                                        <option>Software Question</option>
+                                    <Form.Control 
+                                        as='select' 
+                                        name='subject'
+                                        className='browser-default custom-select' 
+                                        value={subject}
+                                        onChange={this.onInputChange}>
+                                            <option>Business Inquiry</option>
+                                            <option>Freelance Project</option>
+                                            <option>Just to say hello!</option>
+                                            <option>Software Question</option>
                                     </Form.Control>
                                 </Form.Group>
 
                                 <Form.Group controlId='formBasicPassword'>
                                     <Form.Label>Email Address</Form.Label>
-                                    <Form.Control type='email' placeholder='username@gmail.com' htmlSize={75} />
+                                    <Form.Control 
+                                        type='email'
+                                        name='email'
+                                        placeholder='username@gmail.com' 
+                                        htmlSize={75}
+                                        value={email}
+                                        onChange={this.onInputChange}
+                                    />
                                     <Form.Text className='text-muted'>
                                         I'll never share your email with anyone else.
                                     </Form.Text>
                                 </Form.Group>
                                 <Form.Group controlId='formBasicCheckbox'>
                                     <Form.Label>Message (Max. 300 words)</Form.Label>
-                                    <Form.Control as='textarea' rows={3} htmlSize={75} />
+                                    <Form.Control 
+                                        as='textarea'
+                                        name='message'
+                                        rows={3} 
+                                        htmlSize={75}
+                                        value={message}
+                                        onChange={this.onInputChange}
+                                    />
                                 </Form.Group>
                                 <Col className='mt-5' md={{ span: 8, offset: 2 }}>
                                     <Form.Group>
