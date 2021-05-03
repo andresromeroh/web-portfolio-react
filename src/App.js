9b8ef960ee1961488d9accf3a8bccbf86d16334c
ANDRESROMEROH <andres.romero@firstfactory.com>
2021-05-03 09:08:45 -0600

    Adding React-Helmet and Open Graph meta tags

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Footer from './components/Footer';
 import NavigationBar from './components/NavigationBar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -7,10 +8,17 @@ import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
+import LinkPreview from './assets/images/link_preview.png';
 
 function App() {
     return (
         <Router>
+            <Helmet>
+                <title>Andresromero.dev</title>
+                <link rel='canonical' href='https://andresromero.dev/' />
+                <meta name='description' content='Andrés Romero - Software Engineer' />
+                <meta property='og:image' content={LinkPreview} />
+            </Helmet>
             <Container className='App'>
                 <NavigationBar />
                 <Switch>
