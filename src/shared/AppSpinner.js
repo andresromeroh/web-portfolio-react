e3c9a9c95809241bfa031f60344c7524aba63df4
Andrés Romero <andresromeroh.cr@gmail.com>
2021-12-04 06:58:26 -0600

    Rollback to old app spinner

--- src/shared/AppSpinner.js
+++ src/shared/AppSpinner.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Spinnercss } from './CustomStyled';
-import RingLoader from 'react-spinners/RingLoader';
+import PacmanLoader from 'react-spinners/PacmanLoader';
 
 export default class AppSpinner extends Component {
     render() {
         return (
             <React.Fragment>
                 <div className="application-spinner">
-                    <RingLoader css={Spinnercss} size={100} loading={true} color='#ef233c' />
+                    <PacmanLoader css={Spinnercss} size={30} loading={true} color='#ef233c' />
                 </div>
             </React.Fragment>
         )
