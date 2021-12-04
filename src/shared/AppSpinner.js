05a8694fd06c83d2f4e49b4f0c381319805f5174
Andrés Romero <andresromeroh.cr@gmail.com>
2021-12-04 06:21:50 -0600

    Update and replace app spinner

--- src/shared/AppSpinner.js
+++ src/shared/AppSpinner.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Spinnercss } from './CustomStyled';
-import PacmanLoader from 'react-spinners/PacmanLoader';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 export default class AppSpinner extends Component {
     render() {
         return (
             <React.Fragment>
                 <div className="application-spinner">
-                    <PacmanLoader css={Spinnercss} size={30} loading={true} color='#ef233c' />
+                    <ClipLoader css={Spinnercss} size={30} loading={true} color='#ef233c' />
                 </div>
             </React.Fragment>
         )
