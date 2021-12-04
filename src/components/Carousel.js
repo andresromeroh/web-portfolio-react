b4ca210ea44bba0900a5b53196377ef5a7f83b5b
Andrés Romero <andresromeroh.cr@gmail.com>
2021-12-04 06:22:44 -0600

    Update carousel main title

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -24,7 +24,7 @@ const Carousel = ({ items }) => {
         <section className='carousel carousel-items'>
             <Row className='pb-5'>
                 <h2>
-                    <Strong>Digital</Strong> Credentials
+                    <Strong>Tech</Strong> Certifications
                 </h2>
             </Row>
             <AliceCarousel
