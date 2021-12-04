76be1610295144504f9ac11a46b522fe498c7819
Andrés Romero <andresromeroh.cr@gmail.com>
2021-12-04 06:43:24 -0600

    Increase spinner size and update style

--- src/shared/AppSpinner.js
+++ src/shared/AppSpinner.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Spinnercss } from './CustomStyled';
-import PuffLoader from 'react-spinners/PuffLoader';
+import RingLoader from 'react-spinners/RingLoader';
 
 export default class AppSpinner extends Component {
     render() {
         return (
             <React.Fragment>
                 <div className="application-spinner">
-                    <PuffLoader css={Spinnercss} size={100} loading={true} color='#ef233c' />
+                    <RingLoader css={Spinnercss} size={80} loading={true} color='#ef233c' />
                 </div>
             </React.Fragment>
         )
